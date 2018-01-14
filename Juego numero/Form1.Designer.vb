ae809356c9ae168a73c0160f16b97f9b64487646
Saulmarti <saulmartivila@gmail.com>
2018-01-15 00:42:29 +0100

    1

---
+++ Juego numero/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonNumero = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxNumero = New System.Windows.Forms.TextBox()
+        Me.ListBoxNumero = New System.Windows.Forms.ListBox()
+        Me.Limpiar = New System.Windows.Forms.Button()
+        Me.Salir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ButtonNumero
+        '
+        Me.ButtonNumero.Location = New System.Drawing.Point(32, 26)
+        Me.ButtonNumero.Name = "ButtonNumero"
+        Me.ButtonNumero.Size = New System.Drawing.Size(208, 30)
+        Me.ButtonNumero.TabIndex = 0
+        Me.ButtonNumero.Text = "Obten un número entre 1 y 100"
+        Me.ButtonNumero.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 86)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Teclea un número:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 119)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Numeros tecleados"
+        '
+        'TextBoxNumero
+        '
+        Me.TextBoxNumero.Location = New System.Drawing.Point(140, 86)
+        Me.TextBoxNumero.Name = "TextBoxNumero"
+        Me.TextBoxNumero.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxNumero.TabIndex = 3
+        '
+        'ListBoxNumero
+        '
+        Me.ListBoxNumero.FormattingEnabled = True
+        Me.ListBoxNumero.Location = New System.Drawing.Point(32, 145)
+        Me.ListBoxNumero.Name = "ListBoxNumero"
+        Me.ListBoxNumero.Size = New System.Drawing.Size(208, 82)
+        Me.ListBoxNumero.TabIndex = 4
+        '
+        'Limpiar
+        '
+        Me.Limpiar.Location = New System.Drawing.Point(32, 234)
+        Me.Limpiar.Name = "Limpiar"
+        Me.Limpiar.Size = New System.Drawing.Size(75, 23)
+        Me.Limpiar.TabIndex = 5
+        Me.Limpiar.Text = "Limpiar"
+        Me.Limpiar.UseVisualStyleBackColor = True
+        '
+        'Salir
+        '
+        Me.Salir.Location = New System.Drawing.Point(164, 234)
+        Me.Salir.Name = "Salir"
+        Me.Salir.Size = New System.Drawing.Size(75, 23)
+        Me.Salir.TabIndex = 6
+        Me.Salir.Text = "Salir"
+        Me.Salir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Salir)
+        Me.Controls.Add(Me.Limpiar)
+        Me.Controls.Add(Me.ListBoxNumero)
+        Me.Controls.Add(Me.TextBoxNumero)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ButtonNumero)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonNumero As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxNumero As TextBox
+    Friend WithEvents ListBoxNumero As ListBox
+    Friend WithEvents Limpiar As Button
+    Friend WithEvents Salir As Button
+End Class
---
+++ Juego numero/Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class Form1
+    Dim randomValue As Integer
+    Dim numero As Integer
+    Dim contador As Integer
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonNumero.Click
+
+        randomValue = Int(1 + Rnd() * 100)
+        ButtonNumero.Enabled = False
+
+        contador = 0
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox_TextChanged(sender As Object, e As EventArgs) Handles TextBoxNumero.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles TextBoxNumero.KeyDown
+        Select Case e.KeyData
+            Case Keys.Enter
+                Calculo()
+                contador = contador + 1
+        End Select
+    End Sub
+
+    Private Sub Calculo()
+
+        numero = TextBoxNumero.Text
+
+        If (numero = randomValue) Then
+            ListBoxNumero.Items.Add("Numero correcto: " + numero.ToString)
+            ListBoxNumero.Items.Add("Numero de intentos: " + contador.ToString)
+        ElseIf (numero > randomValue) Then
+            ListBoxNumero.Items.Add("El numero es menor de " + numero.ToString)
+        ElseIf (numero < randomValue) Then
+            ListBoxNumero.Items.Add("El numero es mayor de " + numero.ToString)
+        End If
+
+        TextBoxNumero.Text = ""
+    End Sub
+
+    Private Sub Limpiar_Click(sender As Object, e As EventArgs) Handles Limpiar.Click
+        TextBoxNumero.Text = ""
+        ListBoxNumero.Items.Clear()
+        ButtonNumero.Enabled = True
+    End Sub
+
+    Private Sub Salir_Click(sender As Object, e As EventArgs) Handles Salir.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+    End Sub
+
+End Class
